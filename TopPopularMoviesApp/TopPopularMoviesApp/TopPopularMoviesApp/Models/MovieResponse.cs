a53967557426f14e2f5c8397acaff8d0188c3070
Alexis Novas <alexismoises@hotmail.com>
2021-09-24 14:35:51 -0400

    refactor models

--- TopPopularMoviesApp/TopPopularMoviesApp/TopPopularMoviesApp/Models/MovieResponse.cs
+++ TopPopularMoviesApp/TopPopularMoviesApp/TopPopularMoviesApp/Models/MovieResponse.cs
@@ -5,39 +5,6 @@ using System.Text.Json.Serialization;
 
 namespace TopPopularMoviesApp.Models
 {
-    public class Movie
-    {
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
-
-        [JsonPropertyName("rank")]
-        public string Rank { get; set; }
-
-        [JsonPropertyName("rankUpDown")]
-        public string RankUpDown { get; set; }
-
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
-
-        [JsonPropertyName("fullTitle")]
-        public string FullTitle { get; set; }
-
-        [JsonPropertyName("year")]
-        public string Year { get; set; }
-
-        [JsonPropertyName("image")]
-        public string Image { get; set; }
-
-        [JsonPropertyName("crew")]
-        public string Crew { get; set; }
-
-        [JsonPropertyName("imDbRating")]
-        public string ImDbRating { get; set; }
-
-        [JsonPropertyName("imDbRatingCount")]
-        public string ImDbRatingCount { get; set; }
-    }
-
     public class MovieResponse
     {
         [JsonPropertyName("items")]
